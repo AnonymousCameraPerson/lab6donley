079e7078eed2b6403c337eb802b908fada9c007d
AnonymousCameraPerson <lucia@luciadonley.com>
2026-05-27 21:58:57 -0400

    Used code from Discord

--- lab6donley/lab6donley.cpp
+++ lab6donley/lab6donley.cpp
@@ -2,38 +2,19 @@
 #include <allegro5\allegro_primitives.h>
 #include "arrow.h";
 #include "bullet.h"
-#include <cstdlib>
-#include <iostream>
 
-
-
-using namespace std;
-
+bool finished = false;
 bool timeOut = false;
 
 
-void* timer(ALLEGRO_THREAD* ptr, void* arg);
+void *timer(ALLEGRO_THREAD* ptr, void* arg);
 
 int main(void)
 {
-	ALLEGRO_THREAD* create2 = NULL;
-	create2 = al_create_thread(timer, NULL);
-
-	while (!timeOut) {
-		if (!timeOut) {
-			al_start_thread(create2);
-		}
-		else {
-			al_destroy_thread(create2);
-		}
-	}
-
-
-
 	arrowClass arrow;
 	bullet mybullet[10];
-	int score = 0;
-	bool redraw = true;
+	int score=0;
+	bool redraw=true;
 	const int FPS = 60;
 
 	//variables
@@ -42,17 +23,28 @@ int main(void)
 	bool done = false;
 
 	//allegro variable
-	ALLEGRO_DISPLAY* display = NULL;
-	ALLEGRO_EVENT_QUEUE* event_queue = NULL;
-	ALLEGRO_TIMER* timer = NULL;
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+
+	time_t startTime, currentTime; //times used to measure elapsed time
+	startTime = time(NULL);
+	currentTime = time(NULL);
+	time_t times = currentTime - startTime;
+	while (times < 30 && !finished) {
+		currentTime = time(NULL);
+		times = currentTime - startTime;
+	}
+	timeOut = true;
+	return NULL;
 
 	//program init
-	if (!al_init())										//initialize Allegro
+	if(!al_init())										//initialize Allegro
 		return -1;
 
 	display = al_create_display(width, height);			//create our display object
 
-	if (!display)										//test display object
+	if(!display)										//test display object
 		return -1;
 
 	//addon init
@@ -66,32 +58,32 @@ int main(void)
 	timer = al_create_timer(1.0 / FPS);
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_clear_to_color(al_map_rgb(0,0,0));
 	arrow.drawArrow();
 	al_flip_display();
 	al_start_timer(timer);
-	while (!done)
+	while(!done)
 	{
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 
-		if (ev.type == ALLEGRO_EVENT_TIMER)
+		if(ev.type == ALLEGRO_EVENT_TIMER)
 		{
 			redraw = true;
-			for (int i = 0;i < 10;i++)
+			for(int i=0;i<10;i++)
 			{
 				if (!mybullet[i].getStatus()) {
 					mybullet[i].fire();
 				}
 			}
 		}
-		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 		{
 			done = true;
 		}
-		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
 		{
-			switch (ev.keyboard.keycode)
+			switch(ev.keyboard.keycode)
 			{
 			case ALLEGRO_KEY_ESCAPE:
 				done = true;
@@ -110,19 +102,19 @@ int main(void)
 				break;
 			}
 		}
-		if (redraw && al_is_event_queue_empty(event_queue))
+		if(redraw && al_is_event_queue_empty(event_queue))
 		{
-			redraw = false;
+			redraw = false; 
 
-			if (arrow.getSpeed() != 0) {
+			if (arrow.getSpeed()!=0){
 				arrow.erase_arrow();
-				arrow.move_arrow(width, height);
+				arrow.move_arrow(width,height);
 			}
 			arrow.drawArrow();
-			for (int i = 0;i < 10;i++)
+			for(int i=0;i<10;i++)
 			{
 				mybullet[i].erase_bullet();
-				score += mybullet[i].move_bullet(arrow.getX(), arrow.getY(), 32, 32, height);
+				score+=mybullet[i].move_bullet(arrow.getX(),arrow.getY(),32,32,height);
 			}
 		}
 		al_flip_display();
@@ -132,18 +124,4 @@ int main(void)
 	al_destroy_display(display);						//destroy our display object
 	system("pause");
 	return 0;
-}
-
-
-void* timer(ALLEGRO_THREAD* ptr, void* arg)
-{
-	time_t startTime, currentTime; //times used to measure elapsed time
-	startTime = time(NULL);
-	currentTime = time(NULL);
-	while (currentTime - startTime < 10)
-	{
-		currentTime = time(NULL);
-	}
-	timeOut = true;
-	return NULL;
 }
